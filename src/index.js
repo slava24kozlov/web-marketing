7ff27d7a17c82a958b1ebcaf8e0c2c439e55eb57
Slava Kozlov <v.kozlov@freshlimesoft.com>
2022-07-31 16:34:27 +0300

    created the footer part and paragraphs partial of  handlebars

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import "./index.scss";
 import "./handlebars/partials/sorting.scss";
 import "./handlebars/partials/pagination.scss";
 import "./handlebars/partials/card.scss";
+import "./handlebars/partials/paragraphs.scss";
+
+document.getElementById("date").innerText = new Date().getFullYear().toString();
