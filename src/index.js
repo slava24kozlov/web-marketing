adeec3c6fc4e447b8672bd4678173a5de2899ab9
Slava Kozlov <v.kozlov@freshlimesoft.com>
2022-08-01 13:05:59 +0300

    added the possibility of pagination and linked the selects

--- src/index.js
+++ src/index.js
@@ -4,4 +4,65 @@ import "./handlebars/partials/pagination.scss";
 import "./handlebars/partials/card.scss";
 import "./handlebars/partials/paragraphs.scss";
 
+/*CURRENT YEAR*/
 document.getElementById("date").innerText = new Date().getFullYear().toString();
+
+/*SORTING*/
+const selectList = document.querySelectorAll(".sorting > label > select");
+
+selectList.forEach(el => el.addEventListener("change", (event) => {
+  selectList.forEach(el => {
+    if (event.target.value !== el.value) {
+      el.value = event.target.value;
+    }
+  })
+}));
+
+/*PAGINATION*/
+let currentValue = 1;
+const paginationList = document.querySelectorAll(".pagination > div");
+const cards = document.getElementById("cards");
+
+function setActiveClass() {
+  paginationList.forEach(page => {
+    if (currentValue === Number(page.textContent)) {
+      page.className = "activePage";
+    } else {
+      page.className = "";
+    }
+  })
+
+  if (currentValue !== 1) {
+    cards.style.visibility = "hidden";
+  } else {
+    cards.style.visibility = "initial";
+  }
+}
+
+function onChangePaginationRight() {
+  if (currentValue === 3) {
+    currentValue = 1
+  } else {
+    currentValue += 1
+  }
+  setActiveClass();
+}
+
+function onChangePaginationLeft() {
+  if (currentValue === 1) {
+    currentValue = 3;
+  } else {
+    currentValue -= 1;
+  }
+  setActiveClass();
+}
+
+document.querySelectorAll(".pagination > button").forEach((button, index) => {
+  if (index === 0) {
+    button.addEventListener("click", onChangePaginationLeft);
+  } else {
+    button.addEventListener("click", onChangePaginationRight);
+  }
+});
+setActiveClass();
+
